d38fa10b88f746e601ceab8f38f2f43038f626f7
moloch291 <danielkomjati1@gmail.com>
2021-04-20 13:36:22 +0200

    quick refactor and rearrangements 3

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect
 import connection
 import data_manager
+# import util
 
 
 app = Flask(__name__)
@@ -14,15 +15,20 @@ answer_headers = data_manager.ANSWER_HEADER
 def main():
     return render_template("list.html",
                            questions=questions,
-                           question_headers=[" ".join(header.capitalize() for header in header.split("_")) for header in question_headers]
+                           question_headers=[" ".join(header.capitalize() for header in header.split("_"))
+                                             for header in question_headers]
                            )
 
 
 @app.route("/question/<question_id>")
 def display_question(question_id):
-    target_question = [question for question in questions if question['id']==question_id][0]
+    target_question = [question for question in questions if question['id'] == question_id][0]
     target_answers = [answer for answer in answers if answer['question_id'] == question_id]
-    return render_template("question.html", question=target_question, answers=target_answers, answer_headers=answer_headers)
+    return render_template("question.html",
+                           question=target_question,
+                           answers=target_answers,
+                           answer_headers=answer_headers
+                           )
 
 
 if __name__ == "__main__":
