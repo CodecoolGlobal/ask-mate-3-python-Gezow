ba6a84b7125106f70a0dbf7a4ba662379b5a42bc
moloch291 <danielkomjati1@gmail.com>
2021-04-21 19:15:31 +0200

    pair programming: refactor

--- server.py
+++ server.py
@@ -4,16 +4,13 @@ import data_manager
 import util
 from datetime import datetime
 
+
 app = Flask(__name__)
-questions = connection.get_all_user_data(data_manager.QUESTION_FILE_PATH)
-question_headers = data_manager.QUESTION_HEADER
-answers = connection.get_all_user_data(data_manager.ANSWER_FILE_PATH)
-answer_headers = data_manager.ANSWER_HEADER
-images = data_manager.IMAGE_DIR_PATH
 
 
 @app.route("/")
 def main():
+    questions = connection.get_all_user_data(data_manager.QUESTION_FILE_PATH)
     if request.args.get("order_by") == "submission_time" and request.args.get("order_direction") == "desc":
         sorted_questions = sorted(questions, reverse=True, key=lambda item: item['submission_time'])
         order = "asc"
@@ -51,108 +48,119 @@ def main():
                            questions=sorted_questions,
                            if_reversed=order,
                            question_headers=[" ".join(header.capitalize() for header in header.split("_"))
-                                             for header in question_headers]
+                                             for header in data_manager.QUESTION_HEADER]
                            )
 
 
 @app.route("/question/<question_id>")
 def display_question(question_id):
-    target_question = util.generate_lst_of_targets(questions, question_id, 'id')[0]
+    answers = connection.get_all_user_data(data_manager.ANSWER_FILE_PATH)
+    target_question = util.generate_lst_of_targets(connection.get_all_user_data(data_manager.QUESTION_FILE_PATH),
+                                                   question_id, 'id')[0]
     if request.args.get("voted") != "True":
         target_question["view_number"] = str(int(target_question["view_number"]) + 1)
     target_answers = util.generate_lst_of_targets(answers, question_id, "question_id")
     return render_template("question.html",
                            question=target_question,
                            answers=sorted(target_answers, reverse=True, key=lambda item: int(item['vote_number'])),
-                           answer_headers=answer_headers,
+                           answer_headers=data_manager.ANSWER_HEADER,
                            question_id=question_id,
-                           IMAGE_DIR_PATH=images
+                           IMAGE_DIR_PATH=data_manager.IMAGE_DIR_PATH
                            )
 
 
 @app.route("/add-question", methods=["GET", "POST"])
 def add_question():
+    questions = connection.get_all_user_data(data_manager.QUESTION_FILE_PATH)
     if request.method == "POST":
         new_question = {}
         new_id = util.generate_id()
         if request.files["image"]:
             image = request.files['image']
             filename = new_id + "." + "".join(image.filename.split(".")[1])
-            image.save(images, filename)
+            image.save(data_manager.IMAGE_DIR_PATH, filename)
         else:
             filename = None
         util.setting_up_dict(new_question, new_id, str(datetime.now()).split(".")[0], 0, filename, None,
-                             question_headers, request.form)
+                             data_manager.QUESTION_HEADER, request.form)
         connection.append_data(questions, new_question)
-        connection.write_data_file(data_manager.QUESTION_FILE_PATH, questions, question_headers)
+        connection.write_data_file(data_manager.QUESTION_FILE_PATH, questions, data_manager.QUESTION_HEADER)
         return redirect("/question/" + new_question["id"])
     return render_template("add-question.html")
 
 
 @app.route("/question/<question_id>/vote_up")
 def vote_up_question(question_id):
+    questions = connection.get_all_user_data(data_manager.QUESTION_FILE_PATH)
     target_question = util.generate_lst_of_targets(questions, question_id, 'id')[0]
     target_question["vote_number"] = str(int(target_question["vote_number"]) + 1)
-    connection.write_data_file(data_manager.QUESTION_FILE_PATH, questions, question_headers)
+    connection.write_data_file(data_manager.QUESTION_FILE_PATH, questions, data_manager.QUESTION_HEADER)
     return redirect("/question/" + question_id + "?voted=True")
 
 
 @app.route("/question/<question_id>/vote_down")
 def vote_down_question(question_id):
+    questions = connection.get_all_user_data(data_manager.QUESTION_FILE_PATH)
     target_question = util.generate_lst_of_targets(questions, question_id, 'id')[0]
     target_question["vote_number"] = str(int(target_question["vote_number"]) - 1)
-    connection.write_data_file(data_manager.QUESTION_FILE_PATH, questions, question_headers)
+    connection.write_data_file(data_manager.QUESTION_FILE_PATH, questions, data_manager.QUESTION_HEADER)
     return redirect("/question/" + question_id + "?voted=True")
 
 
 @app.route("/answer/<answer_id>/vote_up")
 def vote_up_answer(answer_id):
+    answers = connection.get_all_user_data(data_manager.ANSWER_FILE_PATH)
     target_answer = util.generate_lst_of_targets(answers, answer_id, 'id')[0]
     target_answer["vote_number"] = str(int(target_answer["vote_number"]) + 1)
-    connection.write_data_file(data_manager.ANSWER_FILE_PATH, answers, answer_headers)
+    connection.write_data_file(data_manager.ANSWER_FILE_PATH, answers, data_manager.ANSWER_HEADER)
     question_id = target_answer["question_id"]
     return redirect("/question/" + question_id + "?voted=True")
 
 
 @app.route("/answer/<answer_id>/vote_down")
 def vote_down_answer(answer_id):
+    answers = connection.get_all_user_data(data_manager.ANSWER_FILE_PATH)
     target_answer = util.generate_lst_of_targets(answers, answer_id, 'id')[0]
     target_answer["vote_number"] = str(int(target_answer["vote_number"]) - 1)
-    connection.write_data_file(data_manager.ANSWER_FILE_PATH, answers, answer_headers)
+    connection.write_data_file(data_manager.ANSWER_FILE_PATH, answers, data_manager.ANSWER_HEADER)
     question_id = target_answer["question_id"]
     return redirect("/question/" + question_id + "?voted=True")
 
 
 @app.route("/question/<question_id>/new_answer", methods=["GET", "POST"])
 def add_answer(question_id):
+    answers = connection.get_all_user_data(data_manager.ANSWER_FILE_PATH)
     if request.method == "POST":
         new_answer = {}
         util.setting_up_dict(new_answer, util.generate_id(), str(datetime.now()).split(".")[0], 0, question_id,
-                             answer_headers, request.form)
+                             data_manager.ANSWER_HEADER, request.form)
         connection.append_data(answers, new_answer)
-        connection.write_data_file(data_manager.ANSWER_FILE_PATH, answers, answer_headers)
+        connection.write_data_file(data_manager.ANSWER_FILE_PATH, answers, data_manager.ANSWER_HEADER)
         return redirect("/question/" + question_id)
     return render_template("add-answer.html", question_id=question_id)
 
 
 @app.route('/question/<question_id>/delete_question')
 def delete_question(question_id):
+    answers = connection.get_all_user_data(data_manager.ANSWER_FILE_PATH)
+    questions = connection.get_all_user_data(data_manager.QUESTION_FILE_PATH)
     answers_back = []
     for answer in answers:
         if answer['question_id'] != question_id:
             answers_back.append(answer)
-    connection.write_data_file(data_manager.ANSWER_FILE_PATH, answers_back, answer_headers)
+    connection.write_data_file(data_manager.ANSWER_FILE_PATH, answers_back, data_manager.ANSWER_HEADER)
     target_question = util.generate_lst_of_targets(questions, question_id, 'id')[0]
     questions.remove(target_question)
-    connection.write_data_file(data_manager.QUESTION_FILE_PATH, questions, question_headers)
+    connection.write_data_file(data_manager.QUESTION_FILE_PATH, questions, data_manager.QUESTION_HEADER)
     return redirect("/")
 
 
 @app.route('/answer/<answer_id>/delete_answer')
 def delete_answer(answer_id):
+    answers = connection.get_all_user_data(data_manager.ANSWER_FILE_PATH)
     target_answer = util.generate_lst_of_targets(answers, answer_id, 'id')[0]
     answers.remove(target_answer)
-    connection.write_data_file(data_manager.ANSWER_FILE_PATH, answers, answer_headers)
+    connection.write_data_file(data_manager.ANSWER_FILE_PATH, answers, data_manager.ANSWER_HEADER)
     question_id = target_answer['question_id']
     return redirect("/question/" + question_id)
 
