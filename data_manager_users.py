7bf1959a31b9a6ccf3d2062bb08d0735e1491d2d
Gezow <kokenyesdanone@gmail.com>
2021-05-18 14:53:20 +0200

    data_manager restructure

--- data_manager_users.py
+++ data_manager_users.py
@@ -25,3 +25,12 @@ def find_profile_id(cursor, search_parameter, parameter_type):
             WHERE %s = '%s';""" % (parameter_type, search_parameter)
     cursor.execute(query)
     return cursor.fetchone()
+
+
+@database_common.connection_handler
+def get_ordered_users(cursor, filter_type, order):
+    query = """
+            SELECT id, username, email, reputation, image FROM users
+            ORDER BY %s %s;""" % (filter_type, order)
+    cursor.execute(query)
+    return cursor.fetchall()
