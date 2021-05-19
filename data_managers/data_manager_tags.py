ccaac6da59efad8c7092008eb48479eb804605be
Gezow <kokenyesdanone@gmail.com>
2021-05-19 19:42:39 +0200

    10, rename used tables

--- data_managers/data_manager_tags.py
+++ data_managers/data_manager_tags.py
@@ -5,8 +5,8 @@ import database_common
 def find_relevant_tags(cursor, question_id):
     query = """
             SELECT * FROM tags
-            JOIN question_tag ON tags.id = question_tag.tag_id
-            WHERE question_tag.question_id = '%s';
+            JOIN question_tags ON tags.id = question_tags.tag_id
+            WHERE question_tags.question_id = '%s';
             """ % question_id
     cursor.execute(query)
     return cursor.fetchall()
@@ -33,7 +33,7 @@ def add_new_tag(cursor, tag):
 @database_common.connection_handler
 def choose_tag(cursor, question_id, tag):
     query = """
-            INSERT INTO question_tag
+            INSERT INTO question_tags
             (question_id, tag_id) VALUES ('%s','%s')
             """ % (question_id, tag)
     cursor.execute(query)
@@ -52,7 +52,7 @@ def find_tag_id(cursor, tag_name):
 @database_common.connection_handler
 def delete_tag(cursor, question_id, tag_id):
     query = """
-            DELETE FROM question_tag
+            DELETE FROM question_tags
             WHERE question_id = '%s' 
             AND tag_id = '%s'
             """ % (question_id, tag_id)
@@ -62,7 +62,7 @@ def delete_tag(cursor, question_id, tag_id):
 @database_common.connection_handler
 def get_ordered_tags(cursor, filter_type, order):
     query = """
-            SELECT name, (select count(*) from question_tag where tags.id = question_tag.tag_id) as used
+            SELECT name, (select count(*) from question_tags where tags.id = question_tags.tag_id) as used
             FROM tags
             ORDER BY %s %s;""" % (filter_type, order)
     cursor.execute(query)
